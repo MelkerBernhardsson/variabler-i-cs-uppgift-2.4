31f9bcc237546832599d18081d45c3d7412bdb73
Pingvu <nhlpool27@gmail.com>
2023-01-25 10:25:27 +0100

    Add project files.

---
+++ variabler i cs uppgift 2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgift4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skiv in ett tal ");
+            double x = double.Parse(Console.ReadLine()); // i inmatningingen måste du använd "," och inte "." när du skriver decimaltal//
+            Console.WriteLine("skriv ett till tal");
+            double Y = double.Parse(Console.ReadLine());
+            double summa = x + Y;
+            double product = x * Y;
+            Console.WriteLine("summan av dina tal är: " + summa);
+            Console.WriteLine("produkten av dina tal är: " + product);
+        }
+    }
+}
